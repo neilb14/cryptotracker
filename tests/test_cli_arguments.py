d96dd2dad5db7d5a3759c09df012429ea7df5cd3
Neil Bourgeois <neil.bourgeois@gmail.com>
2018-03-24 22:20:38 -0600

    adding a couple more tests to play with arg parser

--- tests/test_cli_arguments.py
+++ tests/test_cli_arguments.py
@@ -8,5 +8,13 @@ class TestCommandLineArgs(unittest.TestCase):
         self.assertEqual('BTC', result.to_currency)
         self.assertEqual(0, result.charge)
 
+    def test_from_currency(self):
+        result = cli.parse_args('--from USD --amount 0.1 --rate 1192'.split())
+        self.assertEqual('USD', result.from_currency)
+
+    def test_to_currency(self):
+        result = cli.parse_args('--to ETH --amount 0.1 --rate 665'.split())
+        self.assertEqual('ETH', result.to_currency)
+
 if __name__ == '__main__':
     unittest.main()
