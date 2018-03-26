bf04d6933d71f3df5ffd3cef6ffe125e2cd66878
Neil Bourgeois <neil.bourgeois@gmail.com>
2018-03-26 07:39:54 -0600

    display summary when no args are passed; round amounts in summary

--- cryptotracker/summary_writer.py
+++ cryptotracker/summary_writer.py
@@ -1,5 +1,7 @@
+
+
 def write(summary):
     results = []
     for currency in summary:
-        results.append('{}:\r\n  Amount: {}\r\n  Fees: {}\r\n'.format(currency, summary[currency]['amount'], summary[currency]['fees']))
+        results.append('{}\r\n  Amount: {}\r\n  Fees: {}\r\n'.format(currency, round(summary[currency]['amount'], 6), round(summary[currency]['fees'], 6)))
     return results
--- run.py
+++ run.py
@@ -5,9 +5,10 @@ from cryptotracker.datastore import Datastore
 def main(argv):
     args = cli.parse_args(argv)
     ds = Datastore(args['dir'])
-    ds.save(row.build(args))
+    if('amount' in args and args['amount'] is not None and 'rate' in args and args['rate'] is not None):
+        ds.save(row.build(args))
     for coin_summary in summary_writer.write(summary.build(ds.read_all())):
-        print(coin_summary)
+        print('{}\r\n'.format(coin_summary))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
