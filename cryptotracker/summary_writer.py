d083067969a66285898a5bfe55bcc0c4b4c1da10
Neil Bourgeois <neil.bourgeois@gmail.com>
2018-03-29 08:07:46 -0600

    added header and aligning columns

--- cryptotracker/summary_writer.py
+++ cryptotracker/summary_writer.py
@@ -13,9 +13,14 @@ def write_color(text, color, bold=False):
     return (colors.BOLD if bold else '') + color + text + colors.ENDC
 
 def write(summary):
-    results = []
+    currency_header = '{:6}'.format('Coin')
+    amount_header = '{:>20}'.format('Amount')
+    fees_header = '{:>20}'.format('Fees')
+    results = [
+        ' '.join([write_color(currency_header, colors.HEADER, True),write_color(amount_header, colors.HEADER, True),write_color(fees_header, colors.HEADER, True)])
+    ]
     for currency in sorted(summary.keys()):
-        amount = '{0:20}'.format(round(summary[currency]['amount'], 6))
-        fees = '{0:20}'.format(round(summary[currency]['fees'], 8))
-        results.append('\t'.join([write_color(currency, colors.HEADER), write_color(amount, colors.OKGREEN), write_color(fees, colors.OKBLUE)]))        
+        amount = '{:20}'.format(round(summary[currency]['amount'], 6))
+        fees = '{:20}'.format(round(summary[currency]['fees'], 8))
+        results.append(' '.join([write_color('{:6}'.format(currency), colors.HEADER), write_color(amount, colors.OKGREEN), write_color(fees, colors.OKBLUE)]))        
     return results
--- tests/test_summary_writer.py
+++ tests/test_summary_writer.py
@@ -3,16 +3,28 @@ from datetime import datetime
 from cryptotracker import summary_writer
 
 class TestDatastore(unittest.TestCase):
+    def test_headers_are_in_summary(self):
+        summary = {}
+        results = summary_writer.write(summary)
+        self.assertEqual(1, len(results))
+        self.assertTrue('Coin' in results[0])
+        self.assertTrue('Amount' in results[0])
+        self.assertTrue('Fees' in results[0])
+
     def test_summary_string_for_single_coin(self):
         summary = {'BTC':{'amount':100,'fees':1}}
         results = summary_writer.write(summary)
-        self.assertEqual(1, len(results))
-        self.assertEqual('\033[95mBTC\x1b[0m\t\033[92m{0:20}\x1b[0m\t\033[94m{1:20}\033[0m'.format(100, 1), results[0])
+        self.assertEqual(2, len(results))
+        self.assertTrue('BTC' in results[1])
+        self.assertTrue('100' in results[1])
 
     def test_summary_string_rounding(self):
         summary = {'BTC':{'amount':100.23456789,'fees':0.00123456789012345}}
         results = summary_writer.write(summary)
-        self.assertEqual('\033[95mBTC\x1b[0m\t\033[92m{0:20}\x1b[0m\t\033[94m{1:20}\033[0m'.format(100.234568, 0.00123457), results[0])
+        self.assertEqual(2, len(results))
+        self.assertTrue('BTC' in results[1])
+        self.assertTrue('100.234568' in results[1])
+        self.assertTrue('0.00123457' in results[1])
 
 if __name__ == '__main__':
     unittest.main()
