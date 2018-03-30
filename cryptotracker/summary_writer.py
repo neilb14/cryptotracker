638d92d9de96f16f6a62e7c0b4c15881ee09e701
Neil Bourgeois <neil.bourgeois@gmail.com>
2018-03-30 16:59:54 -0600

    use currency formats in summary

--- cryptotracker/summary_writer.py
+++ cryptotracker/summary_writer.py
@@ -1,3 +1,4 @@
+from cryptotracker.currencies import currency_factory
 
 class colors:
     HEADER = '\033[95m'
@@ -12,15 +13,18 @@ class colors:
 def write_color(text, color, bold=False):
     return (colors.BOLD if bold else '') + color + text + colors.ENDC
 
+def tab(column):
+    return '{:20}'.format(str(column))
+
 def write(summary):
     currency_header = '{:6}'.format('Coin')
-    amount_header = '{:>20}'.format('Amount')
-    fees_header = '{:>20}'.format('Fees')
+    amount_header = '{:20}'.format('Amount')
+    fees_header = '{:20}'.format('Fees')
     results = [
         ' '.join([write_color(currency_header, colors.HEADER, True),write_color(amount_header, colors.HEADER, True),write_color(fees_header, colors.HEADER, True)])
     ]
     for currency in sorted(summary.keys()):
-        amount = '{:20}'.format(round(summary[currency]['amount'], 6))
-        fees = '{:20}'.format(round(summary[currency]['fees'], 8))
+        amount = tab(currency_factory.create(currency, float(summary[currency]['amount'])))
+        fees = tab(round(summary[currency]['fees'], 8))
         results.append(' '.join([write_color('{:6}'.format(currency), colors.OKBLUE, True), write_color(amount, colors.OKGREEN), write_color(fees, colors.OKBLUE)]))        
     return results
